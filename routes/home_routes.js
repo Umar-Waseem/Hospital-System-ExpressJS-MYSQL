812f05e143137028edb24625ae2b6764da3fda42
Umar-Waseem <umar.waseem@gmail.com>
2022-12-10 19:06:27 +0500

    display all employees and patients on home

--- routes/home_routes.js
+++ routes/home_routes.js
@@ -1,9 +1,47 @@
 const express = require('express')
 const router = express.Router();
 
+var con = require("../handlers/database_handler.js");
 
-router.get('/', (request, res) => {
-    res.sendFile("I:/SEMESTER 5/Database Systems/Database Project/public/html/home.html")
+const bodyParser = require('body-parser');
+const encoder = bodyParser.urlencoded();
+
+router.get('/',encoder ,(request, res) => {
+
+    patientQuery = "SELECT patientID id , patName name, patCNIC cnic, patPhone phoneNo, patAddress address, pass FROM project.patient;"
+    employeeQuery = "SELECT empID id, empCNIC cnic, empName name, empPhone phone, empAddress address, gender, empDOB dob, experience, shiftstart, shiftend, erole role, empsal salary, prating rating FROM project.employee;"
+
+
+    // run both queries and send the resultant data
+    con.query   (patientQuery   , (err, result, fields) => {    
+        if (err) {
+            res.send
+            ({
+                "error": err.name,
+                "errorMessage": err.message,
+                "requestFrom": request.url
+            })
+        }
+        else {
+
+            con.query   (employeeQuery   , (err, result2, fields) => {    
+                if (err) {
+                    res.send
+                    ({
+                        "error": err.name,
+                        "errorMessage": err.message,
+                        "requestFrom": request.url
+                    })
+                }
+                else {
+                    homePath = "I:/SEMESTER 5/Database Systems/Database Project/public/html/home.ejs"
+                    res.render(homePath, {patientData: result, employeeData: result2})
+                }
+            })
+        }
+    })
+    
+
 });
 
 module.exports = router;
