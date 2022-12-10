819266e934200e08a7480a2e349083a33bdbe2a9
Umar-Waseem <umar.waseem@gmail.com>
2022-12-10 15:10:52 +0500

    patient data by post req

--- routes/patient_routes.js
+++ routes/patient_routes.js
@@ -16,10 +16,26 @@ router.get('/', (request, res) => {
 })
 
 
-router.get('/:patientNo', (request, res) => {
+// router.get('/:patientNo', (request, res) => {
 
 
-    var patientNo = request.params.patientNo;
+//     var patientNo = request.params.patientNo;
+
+//     con.query("select p.patientID as PatientNo, p.patName as PatientName, e.empID as DoctorNo, e.empName as DoctorName, r.empName as ConsultantName, e.empDOB, cm.complaintID as ComplaintNo, t.trID as TreatmentNo, t.trstart as DateTreatmentStarted, t.trend as DateTreatmentEnded from appointment a, doctor d, employee e, employee r, consultant c, treatment t, patient p right outer join complaints cm on p.patientID = cm.patientID where p.patientID = ? and e.empID = d.doctorID and r.empID = c.consID and a.complaintID = cm.complaintID and a.consultantID = c.consID and a.doctorID = d.doctorID order by p.patientID", [patientNo], (err, result, fields) => {
+//         if (err)
+//             throw err;
+
+//         res.render("I:/SEMESTER 5/Database Systems/Database Project/public/html/patient.ejs", { data: result });
+
+//     });
+
+// })
+
+router.post('/', encoder, (request, res) => {
+
+    var patientNo = request.body.patientNoInputFieldValue;
+
+    console.log(patientNo);
 
     con.query("select p.patientID as PatientNo, p.patName as PatientName, e.empID as DoctorNo, e.empName as DoctorName, r.empName as ConsultantName, e.empDOB, cm.complaintID as ComplaintNo, t.trID as TreatmentNo, t.trstart as DateTreatmentStarted, t.trend as DateTreatmentEnded from appointment a, doctor d, employee e, employee r, consultant c, treatment t, patient p right outer join complaints cm on p.patientID = cm.patientID where p.patientID = ? and e.empID = d.doctorID and r.empID = c.consID and a.complaintID = cm.complaintID and a.consultantID = c.consID and a.doctorID = d.doctorID order by p.patientID", [patientNo], (err, result, fields) => {
         if (err)
